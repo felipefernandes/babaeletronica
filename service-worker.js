eea425c2cafcda395a0e43c3c5fb81e2847157f9
Felipe Fernandes <felipefernandesweb@gmail.com>
2024-08-17 13:24:54 -0300

    atualizacao do service worker e do index para contornar o bug do https

--- service-worker.js
+++ service-worker.js
@@ -1,19 +1,25 @@
+const MOTION_EYE_URL = 'http://192.168.1.178';
+
 self.addEventListener('install', (event) => {
-    event.waitUntil(
-        caches.open('baby-monitor-v1').then((cache) => {
-            return cache.addAll([
-                '/',
-                '/index.html',
-                '/icon.png'
-            ]);
-        })
-    );
+    self.skipWaiting();
+});
+
+self.addEventListener('activate', (event) => {
+    event.waitUntil(self.clients.claim());
 });
 
 self.addEventListener('fetch', (event) => {
-    event.respondWith(
-        caches.match(event.request).then((response) => {
-            return response || fetch(event.request);
-        })
-    );
+    if (event.request.url.endsWith('/proxy-image')) {
+        event.respondWith(proxyImage());
+    }
 });
+
+async function proxyImage() {
+    try {
+        const response = await fetch(`${MOTION_EYE_URL}/picture/1/current/`);
+        return response;
+    } catch (error) {
+        console.error('Erro ao buscar imagem:', error);
+        return new Response('Erro ao buscar imagem', { status: 500 });
+    }
+}
